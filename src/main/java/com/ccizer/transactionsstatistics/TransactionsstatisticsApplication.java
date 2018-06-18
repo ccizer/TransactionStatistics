0cb0b8892d402871954ef40c049b93f8014d1b5c
Can Çizer <can.cizer@gmail.com>
2018-06-18 19:23:04 +0300

    initial commit

---
+++ src/main/java/com/ccizer/transactionsstatistics/TransactionsstatisticsApplication.java
@@ -0,0 +1,12 @@
+package com.ccizer.transactionsstatistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionsstatisticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionsstatisticsApplication.class, args);
+    }
+}
